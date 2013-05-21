b45fdf1377e490d7b575e78871c74b921af5c251
shayan <shayan@geeks.ltd.uk>
2013-05-21 09:22:37 +0100

    Initial commit for source files

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using GitSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Log2Clip
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var repo = new Repository(@".");
+            var sb = new StringBuilder();
+            //sb.AppendLine(repo.CurrentBranch.CurrentCommit.Message);
+            foreach (var commit in repo.CurrentBranch.CurrentCommit.Ancestors)
+            {
+                sb.Append(commit.Message);
+            }
+
+            Console.WriteLine(sb.ToString());
+            Clipboard.SetText(sb.ToString());
+        }
+    }
+}
